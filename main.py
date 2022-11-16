ea1a7322fe917695772d0711658f0ee39e4eec76
Ali <46873759+Ghoreish@users.noreply.github.com>
2022-11-16 11:17:15 +0330

    added time delay for high speed vps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import re
-import json
+import time
 f = open('IPs.txt', 'a')
 f.close()
 f = open('IPs.txt', 'r')
@@ -10,7 +10,14 @@ iplist = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', ipstring)
 if iplist:
     f = open('result.txt', 'w')
     for i in iplist:
-        r = requests.get('http://ip-api.com/json/' + str(i))
+        t = 0.1
+        while True:
+            r = requests.get('http://ip-api.com/json/' + str(i))
+            if str(r.status_code) == '200':
+                break
+            else:
+                time.sleep(t)
+                t *= 2
         print(r.text)
         f.write(str(r.text.encode('UTF-8'))[2:-1] + '\n')
     f.close()
